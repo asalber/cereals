891572817f5f317f11692b127c44814a75a81f9a
Alfredo Sánchez Alberca <asalber@gmail.com>
2021-10-13 17:33:00 +0200

    Fixed mistake.

--- inst/rkward/plugins/homework.js
+++ inst/rkward/plugins/homework.js
@@ -9,22 +9,22 @@ function calculate () {
 	dni = getString("dni");
 	echo('data(cereals, package="cereals")\n');
 	echo('set.seed(' + dni + 1 + ')\n');
-	echo('df.cereals$calories <- df.cereals$calories + round(runif(length(df.cereals$calories),-2,2))\n');
-	echo('df.cereals$protein <- df.cereals$protein + round(runif(length(df.cereals$protein),0,1),1)\n');
-	echo('df.cereals$fat <- df.cereals$fat + round(runif(length(df.cereals$fat),0,1),1)\n');
-	echo('df.cereals$sodium <- df.cereals$sodium + round(runif(length(df.cereals$sodium),0,5),1)\n');
-	echo('df.cereals$fiber <- df.cereals$fiber + round(runif(length(df.cereals$fiber),0,1),1)\n');
-	echo('df.cereals$carbohydrates <- df.cereals$carbohydrates + round(runif(length(df.cereals$carbohydrates),0,1),1)\n');
-	echo('df.cereals$sugar <- df.cereals$sugar + round(runif(length(df.cereals$sugar),0,1),1)\n');
-	echo('df.cereals$potassium <- df.cereals$potassium + round(runif(length(df.cereals$potassium),0,5),1)\n');
-	echo('df.cereals$adults.assessment <- df.cereals$adults.assessment + round(rnorm(length(df.cereals$adults.assessment)),2)\n');
-	echo('df.cereals$youths.assessment <- df.cereals$youths.assessment + round(rnorm(length(df.cereals$youths.assessment)),2)\n');
-	echo('.GlobalEnv$df.cereals.' + dni + '<- df.cereals\n');
-	echo('rm(df.cereals,envir=.GlobalEnv)\n');
+	echo('cereals$calories <- cereals$calories + round(runif(length(cereals$calories),-2,2))\n');
+	echo('cereals$protein <- cereals$protein + round(runif(length(cereals$protein),0,1),1)\n');
+	echo('cereals$fat <- cereals$fat + round(runif(length(cereals$fat),0,1),1)\n');
+	echo('cereals$sodium <- cereals$sodium + round(runif(length(cereals$sodium),0,5),1)\n');
+	echo('cereals$fiber <- cereals$fiber + round(runif(length(cereals$fiber),0,1),1)\n');
+	echo('cereals$carbohydrates <- cereals$carbohydrates + round(runif(length(cereals$carbohydrates),0,1),1)\n');
+	echo('cereals$sugar <- cereals$sugar + round(runif(length(cereals$sugar),0,1),1)\n');
+	echo('cereals$potassium <- cereals$potassium + round(runif(length(cereals$potassium),0,5),1)\n');
+	echo('cereals$adults.assessment <- cereals$adults.assessment + round(rnorm(length(cereals$adults.assessment)),2)\n');
+	echo('cereals$youths.assessment <- cereals$youths.assessment + round(rnorm(length(cereals$youths.assessment)),2)\n');
+	echo('.GlobalEnv$cereals.' + dni + '<- cereals\n');
+	echo('rm(cereals,envir=.GlobalEnv)\n');
 }
 
 function printout () {
-	echo('rk.header ("Statistics homework data set", parameters=list("Data set name" = "df.cereals.' + dni + '"))\n');
+	echo('rk.header ("Statistics homework data set", parameters=list("Data set name" = "cereals.' + dni + '"))\n');
 	echo('rk.print("A new data set has been created in your workspace.")\n');
 }
 
